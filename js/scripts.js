de8cfd51846ff9afea74b9d96c24bdc4d3f8e477
Oceanity <github@oceanity.net>
2020-09-03 20:22:31 -0500

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+(function () {
+    const nodeConsole = require("console"),
+        myConsole = new nodeConsole.Console(
+            process.stdout,
+            process.stderr
+        ),
+        {
+            log,
+            error
+        } = myConsole
+    remote = require("electron").remote;
+
+    function Init() {
+        let minBtn = document.getElementById("min-btn"),
+            maxBtn = document.getElementById("max-btn"),
+            closeBtn = document.getElementById("close-btn");
+
+        window.addEventListener("resize", (e) => {
+            const window = remote.getCurrentWindow();
+            if (window.isMaximized())
+                document.body.classList.add("maximized");
+            else
+                document.body.classList.remove("maximized");
+        });
+
+        minBtn.addEventListener("click", (e) => {
+            const window = remote.getCurrentWindow();
+            window.minimize();
+        });
+
+        maxBtn.addEventListener("click", (e) => {
+            const window = remote.getCurrentWindow();
+            if (!window.isMaximized())
+                window.maximize();
+            else
+                window.unmaximize();
+        });
+
+        closeBtn.addEventListener("click", (e) => {
+            const window = remote.getCurrentWindow();
+            window.close();
+        });
+    }
+
+    document.onreadystatechange = function () {
+        if (document.readyState == "complete") {
+            Init();
+        }
+    };
+})();
---
+++ main.js
@@ -0,0 +1,34 @@
+const {
+    app,
+    BrowserWindow
+} = require("electron");
+
+function Init() {
+    const win = new BrowserWindow({
+        icon: `${__dirname}/favicon.ico`,
+        frame: false,
+        transparent: false,
+        width: 1280,
+        height: 720,
+        webPreferences: {
+            enableRemoteModule: true,
+            nodeIntegration: true,
+            webviewTag: true,
+        },
+    });
+    win.loadFile("index.html");
+}
+
+app.whenReady().then(Init);
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+
+app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        Init();
+    }
+});
